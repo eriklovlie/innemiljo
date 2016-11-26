88d6a2190abf1bdf8d857bb0f92e6238724571d0
Erik Lovlie <eriklovlie@gmail.com>
2016-11-26 23:33:12 +0000

    Initial sensor code.

---
+++ tempsensor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+
+while True:
+    data = bus.read_i2c_block_data(0x48, 0)
+    msb = data[0]
+    lsb = data[1]
+    print (((msb << 8) | lsb) >> 4) * 0.0625
+    time.sleep(2)
