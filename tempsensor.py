6ae0a7a6dbde2cd1bea509f80f719f1e64ae83f2
Erik Lovlie <eriklovlie@gmail.com>
2016-11-27 15:51:28 +0000

    Working.

--- tempsensor.py
+++ tempsensor.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 import smbus
 import time
+import subprocess
+import sys
 
+api_key = sys.argv[1]
 bus = smbus.SMBus(1)
 
 while True:
     data = bus.read_i2c_block_data(0x48, 0)
     msb = data[0]
     lsb = data[1]
-    print (((msb << 8) | lsb) >> 4) * 0.0625
-    time.sleep(2)
+    ftemp = (((msb << 8) | lsb) >> 4) * 0.0625
+    stemp = "{:.1f}".format(ftemp)
+    print stemp
+    proc = subprocess.Popen([
+        "curl",
+        "--fail",
+        "--header", "X-THINGSPEAKAPIKEY: {}".format(api_key),
+        "-XPOST",
+        "https://api.thingspeak.com/update?field1={}".format(stemp),
+        ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    if proc.returncode != 0:
+        print out
+        print err
+        exit(1)
+    time.sleep(10)
